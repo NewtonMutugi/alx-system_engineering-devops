a345a08a74b86cb1ce1abc8044acfa339e4ba13d
NewtonMutugi <newtonmut197@gmail.com>
2023-06-29 22:22:38 +0300

    Modified 3-re7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # Install nginx using puppet
 
 exec { 'server configuration':
-  provide => shell,
-  command => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
+  provider => shell,
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com;\\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart',
 }
