628e543f406185c90c2d096d966af7d37455e811
NewtonMutugi <newtonmut197@gmail.com>
2023-07-03 22:21:47 +0300

    Modified 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -19,29 +19,32 @@ file { '/etc/nginx/html/index.html':
 # Configure Nginx custom HTTP response header
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => "
-    server {
+  content => "server {
       listen 80 default_server;
       listen [::]:80 default_server;
       root /etc/nginx/html;
       index index.html index.htm;
-
       location /redirect_me {
         return 301 http://youtube.com/UCw4X_zayaSiuVYcqWpiaSWw;
       }
-
       error_page 404 /404.html;
       location /404 {
         root /etc/nginx/html;
         internal;
       }
-
-      add_header X-Served-By $hostname;
     }
   ",
-  notify  => Service['nginx'],
 }
 
+exec {'HTTP header':
+	command => 'sudo sed -i "6i       add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec {'HTTP header2':
+	command => 'sudo sed -i "13i          add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
 # Enable and restart Nginx service
 service { 'nginx':
   ensure  => running,
