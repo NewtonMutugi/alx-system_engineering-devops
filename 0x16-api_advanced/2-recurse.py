044966ff61b776dc587a6a498ca7a9e931fc33e8
NewtonMutugi <newtonmut197@gmail.com>
2023-08-09 00:04:52 +0300

    Added 2-recurse.py

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module that queries list containing the titles of all
+hot articles for a given subreddit.
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """Returns a list containing the titles of all hot articles"""
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(
+        subreddit, after)
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
+    response = requests.get(url, headers=headers).json()
+
+    if response.get("error") == 404:
+        return None
+    for post in response.get("data").get("children"):
+        hot_list.append(post.get("data").get("title"))
+    after = response.get("data").get("after")
+    if after is None:
+        return hot_list
+    return recurse(subreddit, hot_list, after)
