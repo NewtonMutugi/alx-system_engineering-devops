e380e1661400486a9b7993b9ef82f7ce46d0dca3
NewtonMutugi <newtonmut197@gmail.com>
2023-08-09 21:18:35 +0300

    Modified 0-setup_web_static.sh

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,6 @@
 # This is a sample manifest to show how to use strace to debug a problem
 
-exec { 'fix-apache-issue':
-  command     => '/path/to/fix_script.sh',
-  refreshonly => true,
-}
-service { 'apache2':
-  ensure => 'running',
-  enable => true,
-  require => Exec['fix-apache-issue'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
