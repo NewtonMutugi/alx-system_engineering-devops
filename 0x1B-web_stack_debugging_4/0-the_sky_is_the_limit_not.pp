9feb9726ce0e2c673f14c1998282342f1d39fe8a
NewtonMutugi <newtonmut197@gmail.com>
2023-08-17 10:25:35 +0300

    Added 0-the_sky_is_the_limit_not.pp

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,15 @@
+# Puppet increases the limit of request in the nginx server
+
+# Increase the ULIMIT
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# Restart the nginx service
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Exec['fix--for-nginx']
+}
+```
